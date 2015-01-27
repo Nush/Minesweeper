c54d73461b57a2b21ba27cec448c2afc729af049
shun <shun.ozawa34@gmail.com>
2015-01-27 16:31:41 +0900

    プロトタイプ完成

--- scripts/manager.js
+++ scripts/manager.js
@@ -1,8 +1,43 @@
 "use strict";
 $(function(){
-	console.log('マインスイーパーを始めます。');
-	var ms = new MineSweeper($('#stage'));
-	ms.createStage(10,10);
-	ms.setMines(10);
-	console.log(ms);
+	var $vertical   = $('#vertical'),
+		$horizontal = $('#horizontal'),
+		$mine = $('#mine'),
+		$start = $('#start');
+
+
+	var select= '';
+	for(var i = 1; i <= 20;i++){
+		select+='<option value="'+i+'">'+i+'</option>'
+	}
+	$vertical.html(select);
+	$horizontal.html(select);
+	mineSelect(1,1);
+
+
+	$vertical.on('change',function(){
+		mineSelect($horizontal.val(),$vertical.val());
+	});
+	$horizontal.on('change',function(){
+		mineSelect($horizontal.val(),$vertical.val());
+	});
+
+	function mineSelect(x,y){
+		var mineSelect= '';
+		for(var i = 1; i <= x*y;i++){
+			mineSelect+='<option value="'+i+'">'+i+'</option>'
+		}
+		$mine.html(mineSelect);
+	}
+
+	$start.on('click',function(){
+		console.log('マインスイーパーを始めます。');
+		var ms = new MineSweeper($('#mine-table'));
+		ms.createStage(Number($horizontal.val()),Number($vertical.val()));
+		ms.setMines(Number($mine.val()));
+		ms.startGame();
+
+		console.log(ms);
+	});
+
 });
